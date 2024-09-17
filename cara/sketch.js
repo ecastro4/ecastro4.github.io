646bec65299eeac0d178a15903a761150f59bdfa
ecastro4 <ecastro4@iespompeufabra.cat>
2024-09-17 14:28:52 +0200

    Update sketch.js

--- cara/sketch.js
+++ cara/sketch.js
@@ -1,7 +1,11 @@
 function setup() {
+ createCanvas(600,400);
  // put setup code here
 }
 
 function draw() {
+ background(127);
+ fill(255,0,0);
+ ellipse(300,200,220,300);
   // put drawing code here
 }
