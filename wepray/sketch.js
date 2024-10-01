5175444758cfb11b74e0e1cb42250fe3fe095a26
ecastro4 <ecastro4@iespompeufabra.cat>
2024-10-01 21:47:42 +0200

    Update sketch.js

--- wepray/sketch.js
+++ wepray/sketch.js
@@ -7,7 +7,7 @@ function setup() {
 
 function draw() {
  let = vol =mic.getLevel();
- let h = map(vol,0,1,0,255);
+ let h = map(vol,0,1,0,350);
   background(220);
 //mano derecha
   fill(255-h,202,h);
