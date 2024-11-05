44d6dcc31a3079b8c7e478ee65d424cb70124630
ecastro4 <ecastro4@iespompeufabra.cat>
2024-11-05 09:59:58 +0100

    Update sketch.js

--- cara/sketch.js
+++ cara/sketch.js
@@ -16,7 +16,7 @@ function draw() {
  //Perquè afecta nomès al "draw" a diferència de la variable global "mic" que està al principi i afecta a tot el codi.
  //El punt que hi ha entre "mic" i "getLevel" és molt important, s'anomena la sintaxi del punt o "dot syntax" Aquest punt aplica mètode a objectes
  //És un sistema que et permet crear qualseol cosa i que fai qualseol cosa
- let h =map(mic,0,1,0,300); //"map"és una funció molt potent que el que fa és que onverteix
+ let h = map(vol,0,1,0,300); //"map"és una funció molt potent que el que fa és que onverteix
  // El que fa es converit els numeros de volum que esan entre 0 i 1, sempre en una escala proporciona o mapejada
  // Entre es números 0 i 300, vol dir que si el volum  fos 0,5 es converteix  150 i s'emmagatzema en "h"
  // Si el número és 0,33 de volum es convertira en 100 de "h" o alçada de la boca o arc.
@@ -31,10 +31,10 @@ function draw() {
  ellipse(faceX-50,faceY-50,25,leftEyeSize/2);
  ellipse(faceX+50,faceY-50,25,rightEyeSize/2);
  fill(0,0,0);
- arc(faceX,faceY+50,120,h,0,PI); // La variable "h" determina la alçada de la boca 
+ arc(faceX, faceY + 50, 120, h, 0, PI); // La variable "h" determina la alçada de la boca 
 
 
- console.log(frameCount*0.1);
+ console.log(vol);
  // El "console.log" ens serveixen per veure el valor de les variables en cada moment
  //
 }
